92411dc66940ddd8feb5c3acb27822b32a6cf870
Jags <jags14385@users.noreply.github.com>
2020-05-30 22:09:01 +1000

    Added IOService

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,6 +1,13 @@
+import io.IOService;
+import java.util.ArrayList;
+
 public class EntryPoint {
 
   public static void main(String[] args) {
     System.out.println("Initial Run 101");
+
+    IOService ioService = new IOService("src/main/resources/input.csv");
+    ArrayList<String[]> records = ioService.read();
+    System.out.println(records);
   }
 }
