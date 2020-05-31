2e9b8ec4383d9f898fffb8637a25662879082ced
Jags <jags14385@users.noreply.github.com>
2020-05-31 16:54:42 +1000

    Reformatting

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -9,11 +9,9 @@ import validation.TransactionValidator;
 public class EntryPoint {
 
   public static void main(String[] args) {
-    System.out.println("Initial Run 101");
-
     IParser parser = new CCTransactionParser();
     IOService ioService = new CSVIOService("src/main/resources/input.csv", parser);
-    IValidator validator = new TransactionValidator(new BigDecimal(15));
+    IValidator validator = new TransactionValidator(new BigDecimal(100));
     CCFraudCheckService ccFraudCheckService = new CCFraudCheckService(ioService, validator);
     ccFraudCheckService.init();
   }
